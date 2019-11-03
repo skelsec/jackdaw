aac0c9d937dc6f4d45f81c68f19983ea567c717a
SkelSec <info@skelsec.com>
2019-11-03 23:36:51 +0100

    adding ous

--- jackdaw/nest/api/ou.py
+++ jackdaw/nest/api/ou.py
@@ -5,12 +5,12 @@ from jackdaw.dbmodel.adou import JackDawADOU
 def list_ous(domainid):
     db = SQLAlchemy()
     domains = []
-    for uid, sid, name in db.session\
+    for uid, guid, ou in db.session\
                             .query(JackDawADOU)\
                                 .filter_by(ad_id = domainid)\
-                                .with_entities(JackDawADOU.id, JackDawADOU.sid, JackDawADOU.sAMAccountName)\
+                                .with_entities(JackDawADOU.id, JackDawADOU.objectGUID, JackDawADOU.ou)\
                                 .all():
-        domains.append((uid, sid, name))
+        domains.append((uid, guid, ou))
     return domains
 
 def get(domainid, ouid):
@@ -18,7 +18,7 @@ def get(domainid, ouid):
     user = db.session.query(JackDawADOU).get(ouid)
     return user.to_dict()
 
-def get_sid(domainid, sid):
+def get_guid(domainid, guid):
     db = SQLAlchemy()
-    for user in db.session.query(JackDawADOU).filter_by(sid = sid).filter(JackDawADOU.ad_id == domainid).all():
+    for user in db.session.query(JackDawADOU).filter_by(objectGUID = guid).filter(JackDawADOU.ad_id == domainid).all():
         return user.to_dict()
