7420ca8a5e74715033b8e203bfc00b21528c42ba
Zsolt Imre <imrexzsolt@gmail.com>
2019-11-20 08:13:53 +0000

    Updated nest static serve methods.

--- jackdaw/nest/api/server.py
+++ jackdaw/nest/api/server.py
@@ -2,31 +2,16 @@
 from flask import send_file, send_from_directory
 
 def serve_file(filename):
-    response = send_from_directory('site', filename)
+    response = send_from_directory('site/nui/dist', filename)
+    response.direct_passthrough = False
+    return response
+
+def serve_index():
+    response = send_from_directory('site/nui/dist', 'index.html')
     response.direct_passthrough = False
     return response
 
 def serve_favico():
     response = send_from_directory('site/static/img', 'favicon.ico')
-    response.direct_passthrough = False
-    return response
-
-def serve_js(filename):
-    response = send_from_directory('site/static/js', filename)
-    response.direct_passthrough = False
-    return response
-
-def serve_css(filename):
-    response = send_from_directory('site/static/css', filename)
-    response.direct_passthrough = False
-    return response
-
-def serve_img(filename):
-    response = send_from_directory('site/static/img', filename)
-    response.direct_passthrough = False
-    return response
-
-def serve_home():
-    response = send_from_directory('site/templates', 'index.html')
     response.direct_passthrough = False
     return response
