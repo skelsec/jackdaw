e32c3c74768a2454ab0c2122e674f2b4149f6d48
skelsec <info@skelsec.com>
2020-06-15 12:20:05 -0700

    dns timeout

--- jackdaw/gatherer/rdns/rdns.py
+++ jackdaw/gatherer/rdns/rdns.py
@@ -7,16 +7,17 @@ from jackdaw.gatherer.rdns.udpwrapper import UDPClient
 import os
 
 class RDNS:
-	def __init__(self, server = '8.8.8.8', protocol = 'TCP', cache = True):
+	def __init__(self, server = '8.8.8.8', protocol = 'TCP', cache = True, timeout = 1):
 		self.server = server
 		self.protocol = protocol
 		self.cache = {}
+		self.timeout = timeout
 
 	async def lookup(self, hostname, dnstype = DNSType.A):
 		try:
 			question = DNSQuestion.construct(str(hostname), dnstype, DNSClass.IN, qu = False)
 			if self.protocol == 'TCP':
-				reader, writer = await asyncio.open_connection(self.server, 53)
+				reader, writer = await asyncio.wait_for(asyncio.open_connection(self.server, 53), self.timeout)
 		
 				packet = DNSPacket.construct(
 							TID = os.urandom(2), 
@@ -47,7 +48,7 @@ class RDNS:
 				
 						
 			if self.protocol == 'TCP':
-				reader, writer = await asyncio.open_connection(self.server, 53)
+				reader, writer = await asyncio.wait_for(asyncio.open_connection(self.server, 53), self.timeout)
 		
 				packet = DNSPacket.construct(
 							TID = tid, 
