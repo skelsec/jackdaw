c1f63a811d5867f3b67f56f4ce68f783a6e5193f
skelsec <info@skelsec.com>
2020-11-28 00:11:06 +0100

    gui fixes, upping version

--- jackdaw/_version.py
+++ jackdaw/_version.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.2.27"
+__version__ = "0.2.28"
 __banner__ = \
 """
 # jackdaw %s 
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
 	),
 	install_requires=[
 		'aiosmb>=0.2.32',
-		'msldap>=0.3.20',
+		'msldap>=0.3.22',
 		'sqlalchemy<1.4',
 		'tqdm',
 		'networkx',
